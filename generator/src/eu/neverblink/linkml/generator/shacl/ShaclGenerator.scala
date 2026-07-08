b8ba352599e5b23312fdc66fb5737c994c6add14
kacper <kacper.grzymkowski@neverblink.eu>
2026-07-08 15:01:03 +0200

    shacl union and fixings

--- generator/src/eu/neverblink/linkml/generator/shacl/ShaclGenerator.scala
+++ generator/src/eu/neverblink/linkml/generator/shacl/ShaclGenerator.scala
@@ -1,18 +1,23 @@
 package eu.neverblink.linkml.generator.shacl
 
 import eu.neverblink.linkml.generator.rdf.*
-import eu.neverblink.linkml.schemaview.{ClassView, EnumView, SchemaView, TypeView}
+import eu.neverblink.linkml.metamodel.{AnonymousSlotExpression, SlotDefinition, SlotExpression}
+import eu.neverblink.linkml.runtime.Reference
+import eu.neverblink.linkml.schemaview.SchemaView.defaultRangeResolved
+import eu.neverblink.linkml.schemaview.{ClassView, ElementView, EnumView, SchemaView, SlotView, TypeView}
 
 class ShaclGenerator(using sv: SchemaView) {
 
   /** Generates SHACL rules as RDF model that is represented as a tuple of sequences of namespaces
     * and triple
+    *
     * @param enforceOpenShapes
     *   A flag that enforces all shapes to be open (turned off by default)
     * @param onlyClassesFromRootSchema
     *   Whether to include only classes from the root schema (turned off by default). This is useful
     *   if you intend to generate SHACL shapes for each schema file separately, and you don't need
     *   the imported classes to be included in the generated SHACL shapes.
+    *
     * @return
     *   a tuple of sequences of namespaces and triples
     */
@@ -35,6 +40,104 @@ class ShaclGenerator(using sv: SchemaView) {
     def blankNode(): BlankNode = {
       blankNodeCounter += 1
       BlankNode(blankNodeCounter.toString)
+    }
+
+    def processSlotExpr(
+        slotView: SlotView,
+        slotExpression: SlotExpression,
+        property: Resource,
+    ): Unit = {
+      slotExpression match {
+        case expression: AnonymousSlotExpression =>
+          expression.description match {
+            case Some(d) => addTriple(property, Shacl.description, Literal(d, XmlSchema.string))
+            case _ =>
+          }
+          if (!expression.multivalued) addTriple(property, Shacl.maxCount, Literal.one)
+          if (expression.required) addTriple(property, Shacl.minCount, Literal.one)
+        case definition: SlotDefinition =>
+          definition.description match {
+            case Some(d) => addTriple(property, Shacl.description, Literal(d, XmlSchema.string))
+            case _ =>
+          }
+          if (!definition.multivalued) addTriple(property, Shacl.maxCount, Literal.one)
+          if (definition.required) addTriple(property, Shacl.minCount, Literal.one)
+        case _ => throw RuntimeException("slot expression not one of anon/def")
+      }
+      slotExpression.range.getOrElse(slotView.definingSchema.defaultRangeResolved)
+        .asInstanceOf[Reference[ElementView[?]]].resolve.foreach {
+          case typeView: TypeView =>
+            val isIri = typeView.isIri || slotExpression.implicitPrefix.isDefined
+            if (!isIri) addTriple(property, Shacl.datatype, Iri(typeView.uriStr))
+
+            val nodeKind =
+              if (isIri) Shacl.IRI
+              else Shacl.Literal
+            addTriple(property, Shacl.nodeKind, nodeKind)
+          case classView: ClassView =>
+            val cdUri = classView.uriStr
+            val isLinkmlAny = cdUri == "https://w3id.org/linkml/Any"
+            if (!isLinkmlAny) {
+              addTriple(property, Shacl.`class`, Iri(cdUri))
+              addTriple(property, Shacl.nodeKind, Shacl.BlankNodeOrIRI)
+            }
+          case enumView: EnumView =>
+            val permissibleValues =
+              enumView._enum.permissibleValues.values.foldRight(Rdf.nil: Resource) { (pv, acc) =>
+                val listNode = blankNode()
+                pv.meaning match {
+                  case Some(m) =>
+                    addTriple(
+                      listNode,
+                      Rdf.first,
+                      Iri(m.uri(using enumView.definingPrefixResolver)),
+                    )
+                    addTriple(listNode, Rdf.rest, acc)
+                  case _ =>
+                    addTriple(
+                      listNode,
+                      Rdf.first,
+                      Literal(pv.text),
+                    )
+                    addTriple(listNode, Rdf.rest, acc)
+                }
+                listNode
+              }
+            addTriple(property, Shacl.in, permissibleValues)
+          case _ => throw RuntimeException(s"Couldn't map range ${slotExpression.range}")
+        }
+      if slotExpression.anyOf.nonEmpty then {
+        val or
+        addTriple(property, Shacl.or)
+        slotExpression.anyOf.foreach { anonExpr => {
+
+        }
+        }
+      }
+
+    }
+
+    def processSlot(s: SlotView, order: Int, propertyDomain: Iri): Unit = {
+      val slot = s.slot
+      processSlotExpr(s, slot, propertyDomain)
+      val property = blankNode()
+      addTriple(propertyDomain, Shacl.property, property)
+      addTriple(property, Shacl.order, Literal(order.toString, XmlSchema.integer))
+      addTriple(property, Shacl.path, Iri(s.uriStr))
+    }
+
+    def addShaclList(values: Seq[Node]): Option[BlankNode] = {
+      if values.isEmpty then return None
+      val start = blankNode()
+      val firstNode = values.head
+      addTriple(start, Rdf.first, firstNode)
+      //todo fresh brain for this
+      var next = blankNode()
+      values.foldLeft(()) { (_, value) =>
+        addTriple(next, Rdf.rest, next)
+        next = next
+      }
+      Some(start)
     }
 
     val isEmitted = sv.root.defaultPrefix.foldLeft(
@@ -85,68 +188,8 @@ class ShaclGenerator(using sv: SchemaView) {
         addTriple(ignoredId, Rdf.rest, Rdf.nil)
       } else addTriple(ignoredProperties, Rdf.rest, Rdf.nil)
       var order = 0
-      c.derivedAttributes.values.filter(!_.inner.identifier).foreach { s =>
-        val property = blankNode()
-        addTriple(classNameIri, Shacl.property, property)
-        s.derivedRangeView.resolve.foreach {
-          case typeView: TypeView =>
-            val isIri = typeView.isIri || s.slot.implicitPrefix.isDefined
-            if (!isIri) addTriple(property, Shacl.datatype, Iri(typeView.uriStr))
-            s.slot.description match {
-              case Some(d) => addTriple(property, Shacl.description, Literal(d, XmlSchema.string))
-              case _ =>
-            }
-            if (!s.slot.multivalued) addTriple(property, Shacl.maxCount, Literal.one)
-            if (s.slot.required) addTriple(property, Shacl.minCount, Literal.one)
-            val nodeKind =
-              if (isIri) Shacl.IRI
-              else Shacl.Literal
-            addTriple(property, Shacl.nodeKind, nodeKind)
-          case classView: ClassView =>
-            val cdUri = classView.uriStr
-            val isLinkmlAny = cdUri == "https://w3id.org/linkml/Any"
-            if (!isLinkmlAny) addTriple(property, Shacl.`class`, Iri(cdUri))
-            s.slot.description match {
-              case Some(d) => addTriple(property, Shacl.description, Literal(d, XmlSchema.string))
-              case _ =>
-            }
-            if (!s.slot.multivalued) addTriple(property, Shacl.maxCount, Literal.one)
-            if (s.slot.required) addTriple(property, Shacl.minCount, Literal.one)
-            if (!isLinkmlAny) addTriple(property, Shacl.nodeKind, Shacl.BlankNodeOrIRI)
-          case enumView: EnumView =>
-            s.slot.description match {
-              case Some(d) => addTriple(property, Shacl.description, Literal(d, XmlSchema.string))
-              case _ =>
-            }
-            val permissibleValues =
-              enumView._enum.permissibleValues.values.foldRight(Rdf.nil: Resource) { (pv, acc) =>
-                val listNode = blankNode()
-                pv.meaning match {
-                  case Some(m) =>
-                    addTriple(
-                      listNode,
-                      Rdf.first,
-                      Iri(m.uri(using enumView.definingPrefixResolver)),
-                    )
-                    addTriple(listNode, Rdf.rest, acc)
-                  case _ =>
-                    addTriple(
-                      listNode,
-                      Rdf.first,
-                      Literal(pv.text),
-                    )
-                    addTriple(listNode, Rdf.rest, acc)
-                }
-                listNode
-              }
-            addTriple(property, Shacl.in, permissibleValues)
-            if (!s.slot.multivalued) addTriple(property, Shacl.maxCount, Literal.one)
-            if (s.slot.required) addTriple(property, Shacl.minCount, Literal.one)
-          case _ => throw RuntimeException(s"Couldn't map range ${s.derivedRangeView}")
-        }
-        addTriple(property, Shacl.order, Literal(order.toString, XmlSchema.integer))
-        addTriple(property, Shacl.path, Iri(s.uriStr))
-        order += 1
+      c.derivedAttributes.values.filter(!_.inner.identifier).foreach { x =>
+        processSlot(x, order, classNameIri); order += 1
       }
       addTriple(classNameIri, Shacl.targetClass, classNameIri)
     }
@@ -168,6 +211,7 @@ object Shacl {
   val maxCount: Iri = Iri("http://www.w3.org/ns/shacl#maxCount")
   val minCount: Iri = Iri("http://www.w3.org/ns/shacl#minCount")
   val nodeKind: Iri = Iri("http://www.w3.org/ns/shacl#nodeKind")
+  val or: Iri = Iri("http://www.w3.org/ns/shacl#or")
   val order: Iri = Iri("http://www.w3.org/ns/shacl#order")
   val path: Iri = Iri("http://www.w3.org/ns/shacl#path")
   val property: Iri = Iri("http://www.w3.org/ns/shacl#property")
