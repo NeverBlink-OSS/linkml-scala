f8a5f78389a2d5a48912938e540fbc202182ef21
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2026-08-08 07:16:29 +0200

    LNK-153 Reduce allocations when generating LinkML

--- generator/src/eu/neverblink/linkml/generator/linkml/LinkMlGenerator.scala
+++ generator/src/eu/neverblink/linkml/generator/linkml/LinkMlGenerator.scala
@@ -23,49 +23,62 @@ class LinkMlGenerator(using sv: SchemaView) {
       pruningMode: PruningMode = PruningMode.treeRoot(None),
       skipClassDerivation: Boolean = false,
   ): SchemaDefinitionImpl = {
-    val query = if skipClassDerivation then pruningMode.underivedQuery()
-    else pruningMode.derivedQuery(false, false)
-
+    val query =
+      if (skipClassDerivation) pruningMode.underivedQuery()
+      else pruningMode.derivedQuery(false, false)
     sv.root.asInstanceOf[SchemaDefinitionImpl].copy(
-      imports = Seq.empty,
-      classes = {
-        val toInclude = sv.classes.filter((_, v) => query.reachable(v))
-        if skipClassDerivation then
-          toInclude.map((k, v) =>
-            k -> v.cls.impl.copy(
-              classUri = Some(v.uriOrCurie),
-              fromSchema = Some(v.definingSchema.id),
+      imports = Nil,
+      classes =
+        if (skipClassDerivation) {
+          sv.classes.collect {
+            case (k, v) if query.reachable(v.cls) =>
+              (
+                k,
+                v.cls.impl.copy(
+                  classUri = new Some(v.uriOrCurie),
+                  fromSchema = new Some(v.definingSchema.id),
+                ),
+              )
+          }
+        } else {
+          sv.classes.collect {
+            case (k, v) if query.reachable(v.cls) =>
+              (k, v.materialize)
+          }
+        },
+      types = sv.types.collect {
+        case (k, v) if query.reachable(v._type) =>
+          (
+            k,
+            v._type.impl.copy(
+              typeUri = new Some(v.uriOrCurie),
+              fromSchema = new Some(v.definingSchema.id),
             ),
           )
-        else toInclude.map((k, v) => k -> v.materialize)
       },
-      types = sv.types
-        .collect {
-          case (k, v) if query.reachable(v.inner) =>
-            k -> v.inner.impl.copy(
-              typeUri = Some(v.uriOrCurie),
-              fromSchema = Some(v.definingSchema.id),
-            )
-        },
-      enums = sv.enums
-        .collect {
-          case (k, v) if query.reachable(v.inner) =>
-            k -> v.inner.impl.copy(
-              enumUri = Some(v.uriOrCurie),
-              fromSchema = Some(v.definingSchema.id),
-            )
-        },
+      enums = sv.enums.collect {
+        case (k, v) if query.reachable(v._enum) =>
+          (
+            k,
+            v._enum.impl.copy(
+              enumUri = new Some(v.uriOrCurie),
+              fromSchema = new Some(v.definingSchema.id),
+            ),
+          )
+      },
       slotDefinitions =
-        if skipClassDerivation then
-          sv.slotDefinitions
-            .collect {
-              case (k, v) if query.reachable(v.inner) =>
-                k -> v.inner.impl.copy(
-                  slotUri = Some(v.uriOrCurie),
-                  fromSchema = Some(v.definingSchema.id),
-                )
-            }
-        else Map.empty,
+        if (skipClassDerivation) {
+          sv.slotDefinitions.collect {
+            case (k, v) if query.reachable(v.slot) =>
+              (
+                k,
+                v.slot.impl.copy(
+                  slotUri = new Some(v.uriOrCurie),
+                  fromSchema = new Some(v.definingSchema.id),
+                ),
+              )
+          }
+        } else Map.empty,
     )
   }
 
@@ -96,11 +109,14 @@ class LinkMlGenerator(using sv: SchemaView) {
 
 object LinkMlGenerator {
   // TODO LNK-48: Don't do these horrible casts
-  extension (classDef: ClassDefinition)
-    private def impl: ClassDefinitionImpl = classDef.asInstanceOf
-  extension (typeDef: TypeDefinition) private def impl: TypeDefinitionImpl = typeDef.asInstanceOf
-  extension (slotDef: SlotDefinition) private def impl: SlotDefinitionImpl = slotDef.asInstanceOf
-  extension (enumDef: EnumDefinition) private def impl: EnumDefinitionImpl = enumDef.asInstanceOf
+  extension (inline classDef: ClassDefinition)
+    private inline def impl: ClassDefinitionImpl = classDef.asInstanceOf
+  extension (inline typeDef: TypeDefinition)
+    private inline def impl: TypeDefinitionImpl = typeDef.asInstanceOf
+  extension (inline slotDef: SlotDefinition)
+    private inline def impl: SlotDefinitionImpl = slotDef.asInstanceOf
+  extension (inline enumDef: EnumDefinition)
+    private inline def impl: EnumDefinitionImpl = enumDef.asInstanceOf
 
   /** Serialization format for LinkML models
     */
