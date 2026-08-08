3eda0bd724c0b6047d0f5647d9d1f6c7c08f7a7a
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2026-08-08 07:16:29 +0200

    LNK-153 More efficient ElementView implementation

--- schemaview/src/eu/neverblink/linkml/schemaview/ElementView.scala
+++ schemaview/src/eu/neverblink/linkml/schemaview/ElementView.scala
@@ -7,6 +7,8 @@ import eu.neverblink.linkml.schemaview
 import eu.neverblink.linkml.schemaview.CollectionForm.{CompactDict, SimpleDict}
 import eu.neverblink.linkml.schemaview.expression.ConstructorExpression
 
+import scala.collection.mutable.ListBuffer
+
 /** Element views provide a rich interface for working with schema elements. They require an
   * implicit [[SchemaView]] and are always linked to a defining schema, which is the schema in which
   * the element was originally defined. This allows you to both have the full context of all
@@ -88,10 +90,15 @@ final case class ClassView(cls: ClassDefinition, definingSchema: SchemaDefinitio
 
   def inner: ClassDefinition = cls
 
-  def uriOrCurie: UriOrCurie =
-    cls.classUri.getOrElse(Uri.synthetic(defaultPrefixUri, Case.PascalCase(cls.name)))
+  def uriOrCurie: UriOrCurie = cls.classUri match {
+    case Some(uri) => uri
+    case _ => Uri.synthetic(defaultPrefixUri, Case.PascalCase(cls.name))
+  }
 
-  override def aliasedName: String = cls.alias.getOrElse(Case.PascalCase(cls.name))
+  override def aliasedName: String = cls.alias match {
+    case Some(name) => name
+    case _ => Case.PascalCase(cls.name)
+  }
 
   /** Derived attributes for this class and the identifier slot of a class, if it has one.
     */
@@ -143,21 +150,24 @@ final case class ClassView(cls: ClassDefinition, definingSchema: SchemaDefinitio
     */
   lazy val attributeViews: Map[String, AttributeView] = {
     derivedAttributes.map((k, slot) =>
-      k -> (slot.derivedRange.resolve.get match {
-        case classView: ClassView =>
-          if classView.isAny then AnyView(slot, this)
-          else if !slot.derivedInlined then
-            ClassReferenceAttributeView(
-              slot,
-              this,
-              classView,
-              classView.identifierView.get,
-            )
-          else ClassInlineAttributeView(slot, this, classView, InlineType(slot))
-        case tv: TypeView => TypeAttributeView(slot, this, tv)
-        case ev: EnumView => EnumAttributeView(slot, this, ev)
-        case x => throw RuntimeException(s"Invalid range: ${cls.name}.${slot.name} -> ${x.name}")
-      }),
+      (
+        k,
+        slot.derivedRange.resolve.get match {
+          case classView: ClassView =>
+            if classView.isAny then new AnyView(slot, this)
+            else if !slot.derivedInlined then
+              new ClassReferenceAttributeView(
+                slot,
+                this,
+                classView,
+                classView.identifierView.get,
+              )
+            else new ClassInlineAttributeView(slot, this, classView, InlineType(slot))
+          case tv: TypeView => new TypeAttributeView(slot, this, tv)
+          case ev: EnumView => new EnumAttributeView(slot, this, ev)
+          case x => throw RuntimeException(s"Invalid range: ${cls.name}.${slot.name} -> ${x.name}")
+        },
+      ),
     )
   }
 
@@ -183,10 +193,25 @@ final case class ClassView(cls: ClassDefinition, definingSchema: SchemaDefinitio
     */
   def subjectType: Option[SubjectType] = identifierView.map(_.subjectType)
 
-  private def getParents(view: ClassView): Iterable[ClassView] =
-    (view.cls.mixins ++ view.cls.isA).flatMap { r =>
-      sv.resolve(r.asInstanceOf[Reference[ClassView]])
+  private def getParents(view: ClassView): Iterable[ClassView] = {
+    val parents = new ListBuffer[ClassView]
+    val cls = view.cls
+    cls.mixins.foreach { r =>
+      sv.resolve(r.asInstanceOf[Reference[ClassView]]) match {
+        case Some(cv) => parents.addOne(cv)
+        case _ =>
+      }
     }
+    cls.isA match {
+      case Some(r) =>
+        sv.resolve(r.asInstanceOf[Reference[ClassView]]) match {
+          case Some(cv) => parents.addOne(cv)
+          case _ =>
+        }
+      case _ =>
+    }
+    parents.toList
+  }
 
   /** Get and dereference all the ancestors (transitive parents) of this class.
     *
@@ -203,8 +228,12 @@ final case class ClassView(cls: ClassDefinition, definingSchema: SchemaDefinitio
     */
   def directSlots: Seq[Reference[SlotDefinition]] = getDirectSlots(cls)
 
-  private def getDirectSlots(cls: ClassDefinition): Seq[Reference[SlotDefinition]] =
-    cls.slots ++ cls.attributes.keys.map(Reference[SlotDefinition])
+  private def getDirectSlots(cls: ClassDefinition): Seq[Reference[SlotDefinition]] = {
+    val slots = new ListBuffer[Reference[SlotDefinition]]
+    slots.addAll(cls.slots)
+    cls.attributes.keys.foreach(a => slots.addOne(Reference[SlotDefinition](a)))
+    slots.toList
+  }
 
   /** Test whether the class or its ancestors have this slot defined as an attribute.
     *
@@ -215,8 +244,10 @@ final case class ClassView(cls: ClassDefinition, definingSchema: SchemaDefinitio
     * @return
     *   True if the slot comes from class attributes, false otherwise
     */
-  def isSlotFromAttributes(slotRef: Reference[SlotDefinition]): Boolean =
-    ancestors(true).exists(anc => anc.cls.attributes.keys.exists(_ == slotRef.value))
+  def isSlotFromAttributes(slotRef: Reference[SlotDefinition]): Boolean = {
+    val name = slotRef.value
+    ancestors(true).exists(anc => anc.cls.attributes.keys.exists(_.equals(name)))
+  }
 
   /** Derive a slot for a class, taking into account the `slotUsage` and `attributes` for the class
     * and its ancestors, as well as the schema top-level slots and its ancestors.
@@ -272,7 +303,7 @@ final case class ClassView(cls: ClassDefinition, definingSchema: SchemaDefinitio
     )
     // Apply the original schema as the defining schema, so that default prefix / default range
     // resolution still works as defined in the original schema file.
-    SlotView(finalSlot, source.definingSchema)
+    new SlotView(finalSlot, source.definingSchema)
   }
 
   /** Test whether this class definition has an identifier slot
@@ -291,35 +322,37 @@ final case class ClassView(cls: ClassDefinition, definingSchema: SchemaDefinitio
     */
   def treeRootInlineType(overrideType: Option[String]): InlineType =
     val value = overrideType.orElse {
-      cls.extensions.get("tree_root_as").map(_.extensionValue.value.trim)
+      cls.extensions.get("tree_root_as").map(_.extensionValue.value.strip)
     }
-    lazy val msg =
-      s"Class '$name' has 'tree_root_as: ${value.get}', but it cannot be inlined in this form"
     value.map(v =>
-      Case.camelCase(v).strip() match {
+      Case.camelCase(v) match {
         case "plain" => InlineType.plain
         case "optional" => InlineType.optional
         case "list" => InlineType.list
         case "simpleDict" =>
           collectionForm match {
             case form: SimpleDict => InlineType.dict(form)
-            case _ => throw new IllegalArgumentException(msg)
+            case _ =>
+              throw new IllegalArgumentException(
+                s"Class '$name' has 'tree_root_as: $v', but it cannot be inlined in simpleDict form",
+              )
           }
         case "compactDict" =>
           collectionForm match {
             case form: DictForm =>
               // override simpledict inference to
               InlineType.dict(CompactDict(form.key))
-            case _ => throw new IllegalArgumentException(msg)
+            case _ =>
+              throw new IllegalArgumentException(
+                s"Class '$name' has 'tree_root_as: $v', but it cannot be inlined in compactDict form",
+              )
           }
-        case _ if overrideType.isEmpty =>
-          throw new IllegalArgumentException(
-            s"Class '$name' has unknown 'tree_root_as' extension value: '$v'",
-          )
         case _ =>
-          throw new IllegalArgumentException(
-            s"Class '$name' has unknown 'tree_root_as' override value: '$v'",
-          )
+          throw new IllegalArgumentException({
+            if (overrideType.isEmpty)
+              s"Class '$name' has unknown 'tree_root_as' extension value: '$v'"
+            else s"Class '$name' has unknown 'tree_root_as' override value: '$v'"
+          })
       },
     ).getOrElse(InlineType.plain)
 
@@ -327,23 +360,25 @@ final case class ClassView(cls: ClassDefinition, definingSchema: SchemaDefinitio
     * attributes, and clears any inheritance slots. Additionally, sets the class uri using
     * [[SchemaView]] logic.
     */
-  def materialize: ClassDefinitionImpl = {
+  def materialize: ClassDefinitionImpl =
     inner.asInstanceOf[ClassDefinitionImpl].copy(
-      classUri = Some(uriOrCurie),
+      classUri = new Some(uriOrCurie),
       isA = None,
-      mixins = Seq.empty,
+      mixins = Nil,
       attributes = derivedAttributes.map((slotKey, slot) =>
-        slotKey -> slot.inner.asInstanceOf[SlotDefinitionImpl].copy(
-          isA = None,
-          mixins = Seq.empty,
-          fromSchema = Some(slot.definingSchema.id),
+        (
+          slotKey,
+          slot.inner.asInstanceOf[SlotDefinitionImpl].copy(
+            isA = None,
+            mixins = Nil,
+            fromSchema = new Some(slot.definingSchema.id),
+          ),
         ),
       ),
-      slots = Seq.empty,
+      slots = Nil,
       slotUsage = Map.empty,
-      fromSchema = Some(definingSchema.id),
+      fromSchema = new Some(definingSchema.id),
     )
-  }
 }
 
 final case class SlotView(slot: SlotDefinition, definingSchema: SchemaDefinition)(using
@@ -353,12 +388,17 @@ final case class SlotView(slot: SlotDefinition, definingSchema: SchemaDefinition
 
   def inner: SlotDefinition = slot
 
-  override def aliasedName: String = slot.alias.getOrElse(Case.deSpaceCase(slot.name))
+  override def aliasedName: String = slot.alias match {
+    case Some(name) => name
+    case _ => Case.deSpaceCase(slot.name)
+  }
 
   /** Resolved URI string for the implicit_prefix metaslot for this slot, if defined
     */
-  def implicitPrefixReference: Option[String] =
-    slot.implicitPrefix.flatMap(definingPrefixResolver.resolvePrefix)
+  def implicitPrefixReference: Option[String] = slot.implicitPrefix match {
+    case Some(p) => definingPrefixResolver.resolvePrefix(p)
+    case _ => None
+  }
 
   /** Get and dereference the direct parents (mixins + inheritance) of this slot
     *
@@ -367,8 +407,24 @@ final case class SlotView(slot: SlotDefinition, definingSchema: SchemaDefinition
     */
   def parents: Iterable[SlotDefinition] = getParents(slot)
 
-  private def getParents(slot: SlotDefinition): Iterable[SlotDefinition] =
-    (slot.mixins ++ slot.isA).flatMap(sv.resolve)
+  private def getParents(slot: SlotDefinition): Iterable[SlotDefinition] = {
+    val parents = new ListBuffer[SlotDefinition]
+    slot.mixins.foreach { r =>
+      sv.resolve(r) match {
+        case Some(cv) => parents.addOne(cv)
+        case _ =>
+      }
+    }
+    slot.isA match {
+      case Some(r) =>
+        sv.resolve(r) match {
+          case Some(cv) => parents.addOne(cv)
+          case _ =>
+        }
+      case _ =>
+    }
+    parents.toList
+  }
 
   /** Get and dereference all the ancestors (transitive parents) of this slot.
     *
@@ -398,8 +454,10 @@ final case class SlotView(slot: SlotDefinition, definingSchema: SchemaDefinition
     * Make sure you use this method after class/slot derivation is performed.
     */
   def derivedRange: Reference[ElementView[?, ?]] =
-    slot.range.getOrElse(sv.getDefaultRange(definingSchema))
-      .asInstanceOf[Reference[ElementView[?, ?]]]
+    (slot.range match {
+      case Some(r) => r
+      case _ => sv.getDefaultRange(definingSchema)
+    }).asInstanceOf[Reference[ElementView[?, ?]]]
 
   /** Get the default value of this slot if the `ifabsent` metaslot is defined.
     *
@@ -410,7 +468,10 @@ final case class SlotView(slot: SlotDefinition, definingSchema: SchemaDefinition
     *   The range of the slot. Obtain it from [[SlotView.derivedRange]].
     */
   def ifAbsent[R](range: ElementView[?, R]): Option[R] =
-    slot.ifabsent.flatMap(range.evaluateConstructor)
+    slot.ifabsent match {
+      case Some(ia) => range.evaluateConstructor(ia)
+      case _ => None
+    }
 
   /** Get the URI of this slot, using the default prefix of the implicit [[SchemaView]] if not
     * explicitly defined.
@@ -421,7 +482,10 @@ final case class SlotView(slot: SlotDefinition, definingSchema: SchemaDefinition
 private object SlotView:
   // Exposed for slot derivation in ClassView.
   def uri(slotUri: Option[UriOrCurie], slotName: String, context: ElementView[?, ?]): UriOrCurie =
-    slotUri.getOrElse(Uri.synthetic(context.defaultPrefixUri, Case.deSpaceCase(slotName)))
+    slotUri match {
+      case Some(uri) => uri
+      case _ => Uri.synthetic(context.defaultPrefixUri, Case.deSpaceCase(slotName))
+    }
 
 final case class EnumView(_enum: EnumDefinition, definingSchema: SchemaDefinition)(using
     sv: SchemaView,
@@ -435,20 +499,35 @@ final case class EnumView(_enum: EnumDefinition, definingSchema: SchemaDefinitio
   override private[schemaview] def evaluateConstructor(expr: String): Option[PermissibleValue] =
     Some(ConstructorExpression.evaluateEnum(expr, this))
 
-  def uriOrCurie: UriOrCurie =
-    _enum.enumUri.getOrElse(Uri.synthetic(defaultPrefixUri, Case.PascalCase(_enum.name)))
+  def uriOrCurie: UriOrCurie = _enum.enumUri match {
+    case Some(uri) => uri
+    case _ => Uri.synthetic(defaultPrefixUri, Case.PascalCase(_enum.name))
+  }
 
   /** Permissible values of this enum and their (possibly synthetic) meanings */
   lazy val derivedValues: Seq[(pv: PermissibleValue, meaning: UriOrCurie)] =
-    _enum.permissibleValues.values.map(x =>
-      x -> x.meaning.getOrElse(Uri.synthetic(defaultPrefixUri, Case.deSpaceCase(x.text))),
-    ).toSeq
+    _enum.permissibleValues.values
+      .foldLeft(new ListBuffer[(pv: PermissibleValue, meaning: UriOrCurie)]) { (acc, x) =>
+        acc.addOne(
+          (
+            x,
+            x.meaning match {
+              case Some(m) => m
+              case _ => Uri.synthetic(defaultPrefixUri, Case.deSpaceCase(x.text))
+            },
+          ),
+        )
+      }.toList
 
   lazy val toMeaning: Map[String, UriOrCurie] =
-    derivedValues.map((x, meaning) => x.text -> meaning).toMap
+    derivedValues.foldLeft(Map.empty[String, UriOrCurie]) { case (acc, (x, meaning)) =>
+      acc.updated(x.text, meaning)
+    }
 
   lazy val fromMeaning: Map[UriOrCurie, String] =
-    derivedValues.map((x, meaning) => meaning -> x.text).toMap
+    derivedValues.foldLeft(Map.empty[UriOrCurie, String]) { case (acc, (x, meaning)) =>
+      acc.updated(meaning, x.text)
+    }
 }
 
 // TODO LNK-63:
@@ -475,11 +554,12 @@ final case class TypeView(_type: TypeDefinition, definingSchema: SchemaDefinitio
       inner.implicitPrefix match {
         case Some(prefix) =>
           val reference =
-            definingPrefixResolver.resolvePrefix(prefix).getOrElse(
-              throw RuntimeException(s"Unknown implicit prefix for type $name: $prefix"),
-            )
-          SubjectType.implicitPrefix(reference)
-        case None => SubjectType.base
+            definingPrefixResolver.resolvePrefix(prefix) match {
+              case Some(p) => p
+              case _ => throw RuntimeException(s"Unknown implicit prefix for type $name: $prefix")
+            }
+          new SubjectType.implicitPrefix(reference)
+        case _ => SubjectType.base
       }
   }
 
@@ -521,15 +601,17 @@ final case class TypeView(_type: TypeDefinition, definingSchema: SchemaDefinitio
 
         case _ => UnknownType
       }
-    case None => UnknownType
+    case _ => UnknownType
   }
 
   /** The [[CoreType]] representation of this type.
     */
   def coreType: CoreType = runtimeType.repr
 
-  def uriOrCurie: UriOrCurie =
-    _type.typeUri.getOrElse(Uri.synthetic(defaultPrefixUri, _type.name))
+  def uriOrCurie: UriOrCurie = _type.typeUri match {
+    case Some(uri) => uri
+    case _ => Uri.synthetic(defaultPrefixUri, _type.name)
+  }
 }
 
 final case class SubsetView(subset: SubsetDefinition, definingSchema: SchemaDefinition)(using
