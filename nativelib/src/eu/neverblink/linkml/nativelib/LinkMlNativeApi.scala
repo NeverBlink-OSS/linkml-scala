b0d22eb1a881316d66937d57f926d6bb41e321db
Ostrzyciel <piotr@neverblink.eu>
2026-09-09 09:47:22 +0200

    cleanup done

--- nativelib/src/eu/neverblink/linkml/nativelib/LinkMlNativeApi.scala
+++ nativelib/src/eu/neverblink/linkml/nativelib/LinkMlNativeApi.scala
@@ -31,9 +31,8 @@ object LinkMlNativeApi {
     *
     *   - 1: initial ABI version.
     *   - 2: added `linkml_build_info`; replaced `tableSchema` with `frictionless`.
-    *   - 3: rebuilt with Scala Native. The isolate arguments were removed.
-    *     `linkml_init_threads` replaces `graal_create_isolate`. `platform` in
-    *     `linkml_build_info` is now `SCALA_NATIVE`.
+    *   - 3: rebuilt with Scala Native. The isolate arguments were removed. `linkml_init_threads`
+    *     replaces `graal_create_isolate`. `platform` in `linkml_build_info` is now `SCALA_NATIVE`.
     */
   final val abiVersion: Int = 3
 
