868d366cad6f40b0b4a6f3d7956fc6e5472df56b
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2026-09-13 07:18:33 +0200

    Fix Scala 3.9 keywords: - remove Scala 2 keywords - comment out soft keywords - add missing keywords - add unit test for keywords - sort keywords alphabetically - more efficient escaping of keywords and prefixing of reserved words
    More efficient escaping of keywords and reserved words
    
    More efficient escaping of keywords and reserved words

--- generator/src/eu/neverblink/linkml/generator/scala/ScalaWords.scala
+++ generator/src/eu/neverblink/linkml/generator/scala/ScalaWords.scala
@@ -1,53 +1,67 @@
 package eu.neverblink.linkml.generator.scala
 
-/** Scala keywords and reserved words. Keywords may be escaped with backticks (`). Reserved words
-  * must be escaped with an underscore instead.
+/** Scala 3.9 LTS keywords and reserved words. Keywords may be escaped with backticks (`). Soft
+  * keywords are listed but commented - they can be used in standard identifiers like class, trait,
+  * method, or fields. Reserved words must be escaped with an underscore instead.
   */
 object ScalaWords {
   val keywords: Set[String] = Set(
     "abstract",
+    // "as", soft keyword, example: import scala.collection.mutable.{Map as MutableMap}
     "case",
     "catch",
     "class",
     "def",
+    // "derives", soft keyword, example: enum Tree[T] derives CanEqual
     "do",
     "else",
+    // "end", soft keyword, example: end MyClass
+    "enum",
+    // "erased", soft keyword, experimental
+    "export",
     "extends",
+    // "extension", soft keyword, example: extension (s: String) def reformat: String = ...
+    "false",
     "final",
     "finally",
     "for",
-    "forSome",
+    "given",
     "if",
     "implicit",
     "import",
+    // "infix", soft keyword, example: infix def add(other: Int): Int
+    // "inline", soft keyword, example: inline def assert(condition: => Boolean): Unit = ...
+    // "into", soft keyword, example: into trait IntoIterable[A] extends Iterable[A]
     "lazy",
     "match",
     "new",
+    "null",
     "object",
+    // "opaque", soft keyword, example: opaque type Logarithm = Double
+    // "open", soft keyword, example: open class BaseComponent
     "override",
     "package",
+    "private",
     "protected",
     "return",
     "sealed",
     "super",
+    "then",
     "this",
     "throw",
     "trait",
+    // "transparent", soft keyword, example: transparent inline def choose(b: Boolean) = ...
+    "true",
     "try",
     "type",
+    // "using", soft keyword, example: def sort[A](list: List[A])(using ord: Ord[A]) = ...
     "val",
     "var",
     "while",
     "with",
     "yield",
-    "inline",
-    "derives",
-    "end",
-    "extension",
-    "using",
-    "as",
   )
-
+  val maxKeywordLength = keywords.foldLeft(0)(_ max _.length)
   val reserved: Set[String] = Set(
     // java.lang.Object
     "hashCode",
@@ -76,4 +90,5 @@ object ScalaWords {
     "LocalizedText",
     "Unknown",
   )
+  val maxReservedLength = reserved.foldLeft(0)(_ max _.length)
 }
